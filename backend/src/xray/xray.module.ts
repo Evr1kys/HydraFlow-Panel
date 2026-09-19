60e6af8811e13f4206c629a3b79b37ce31d789b9
Evr1kys <fixik1009@gmail.com>
2026-09-19 19:48:21 +0300

    feat(xray): use Nodes Agent module

--- backend/src/xray/xray.module.ts
+++ backend/src/xray/xray.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { XrayService } from './xray.service';
 import { XrayController } from './xray.controller';
 import { AuthModule } from '../auth/auth.module';
+import { NodesModule } from '../nodes/nodes.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, NodesModule],
   controllers: [XrayController],
   providers: [XrayService],
   exports: [XrayService],
